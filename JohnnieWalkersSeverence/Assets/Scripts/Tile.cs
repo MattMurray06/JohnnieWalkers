157cc60011dc6e367ca9275129eb953dc63497f4
jc10101010 <josephclarke@live.com>
2025-03-15 13:23:47 +0000

    Added text on cells

--- JohnnieWalkersSeverence/Assets/Scripts/Tile.cs
+++ JohnnieWalkersSeverence/Assets/Scripts/Tile.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {   
     [SerializeField] private Color _normalColor;
-    [SerializeField] private Color _baseColor;
+    [SerializeField] private Color _baseColor; 
+    
     [SerializeField] private Color _offsetColor;
-
+    
     [SerializeField] private Color _highlightColor;
 
-
+    private void Start() {
+        Canvas canvas = GetComponentInChildren<Canvas>();
+        if (canvas != null) {
+            canvas.worldCamera = Camera.main;
+        }
+    }
     public void SetColorOffset() {
         _normalColor = _offsetColor;
         GetComponent<SpriteRenderer>().color = _offsetColor;
